f3d7b5e80908bd57a1e29efb1ef3c4f034074360
kleva-j <kasmickleva@gmail.com>
2023-08-25 17:47:35 +0100

    refactor(web+ui): minor changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button, Header, Avatar, AvatarFallback, AvatarImage } from "ui";
 export default function Page(): JSX.Element {
   return (
     <section className="ui-container ui-flex ui-items-center ui-justify-center ui-gap-x-5 ui-h-screen">
-      <Header text="Web" />
+      <span className="ui-font-sans">Web</span>
       <Button variant="destructive">Button</Button>
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
