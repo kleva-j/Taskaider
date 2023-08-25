77c969e49b1fdd8d3870d8238c3c63b426731704
kleva-j <kasmickleva@gmail.com>
2023-08-25 12:16:28 +0100

    feat: fix and update styles inport

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { Button, Header } from "ui";
+import { Button, Header, Avatar, AvatarFallback, AvatarImage } from "ui";
 
 export default function Page(): JSX.Element {
   return (
-    <section className="container">
+    <section className="ui-container ui-flex ui-items-center ui-justify-center ui-gap-x-5 ui-h-screen">
       <Header text="Web" />
       <Button variant="destructive">Button</Button>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </section>
   );
 }
