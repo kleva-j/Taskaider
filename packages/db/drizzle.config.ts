f501f07dfda91991dba82a528f4dc78168f6c586
kleva-j <kasmickleva@gmail.com>
2023-08-29 18:06:43 +0100

    chore(db): set db config, run and push migrations

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+import type { Config } from "drizzle-kit";
+import { env } from "./env.mjs";
+
+export default {
+  schema: "./src/schema/index.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
+  },
+  driver: "turso",
+  strict: true,
+} satisfies Config;
