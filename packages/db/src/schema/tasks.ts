4c05e78b37355469f6cf72108efd3fc72e58ca8c
kleva-j <kasmickleva@gmail.com>
2023-08-30 14:03:32 +0100

    feat(db): add new fields to tasks table

--- packages/db/src/schema/tasks.ts
+++ packages/db/src/schema/tasks.ts
@@ -1,13 +1,30 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 import { createId } from "@paralleldrive/cuid2";
 import { sql } from "drizzle-orm";
+import { z } from "zod";
+
+export const availableStatus = [
+  "backlog",
+  "todo",
+  "in progress",
+  "done",
+  "cancelled",
+] as const;
+export const defaultTags = ["documentation", "bugs", "feature"] as const;
+export const priorities = ["low", "medium", "high"] as const;
+
+export const StatusEnum = z.enum(availableStatus);
+export const PriorityEnum = z.enum(priorities);
 
 export const tasks = sqliteTable("tasks", {
   id: text("id")
     .$defaultFn(() => createId())
     .primaryKey(),
-  status: text("status", { enum: ["open", "closed"] }).default("open"),
-  description: text("description").default(""),
+  status: text("status", { enum: availableStatus }).default("backlog"),
+  title: text("title", { length: 256 }).default("").notNull(),
+  authorId: text("author_id").default(""),
+  tag: text("tag").default(""),
+  priority: text("priority", { enum: priorities }).default("low"),
   createdAt: integer("created_at", { mode: "timestamp" }).default(
     sql`(strftime('%s', 'now'))`,
   ),
