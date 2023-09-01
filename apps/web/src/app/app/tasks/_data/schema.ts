2bbe9e1c9ad7bd9aed0179742bf4dbc3451c63e9
kleva-j <kasmickleva@gmail.com>
2023-09-01 18:47:43 +0100

    feat(web): add dummy tasks and update schema

--- apps/web/src/app/app/tasks/_data/schema.ts
+++ apps/web/src/app/app/tasks/_data/schema.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const taskSchema = z.object({
   id: z.string(),
   title: z.string(),
-  authorId: z.string(),
+  authorId: z.string().optional(),
   priority: z.enum(["low", "medium", "high"]),
-  label: z.enum(["documentation", "bugs", "feature"]),
-  status: z.enum(["backlog", "todo", "in progress", "done", "cancelled"]),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  label: z.enum(["documentation", "bug", "feature"]),
+  status: z.enum(["backlog", "todo", "in progress", "done", "canceled"]),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
 });
 
 export type Task = z.infer<typeof taskSchema>;
