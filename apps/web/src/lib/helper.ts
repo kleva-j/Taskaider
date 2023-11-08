74249a69caa19eb0479b168579736b232f326665
kleva-j <kasmickleva@gmail.com>
2023-11-08 22:27:22 +0100

    feat: create server action for crud task operations

--- apps/web/src/lib/helper.ts
+++ apps/web/src/lib/helper.ts
@@ -23,3 +23,26 @@ export const filterParams = (params: paramsType) => {
 
 export const isEmpty = (obj: Record<string, any>) =>
   obj && Object.keys(obj).length === 0 && obj.constructor === Object;
+
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString(undefined, options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${time} - ${formattedDate}`;
+}
+
+export function isBase64Image(imageData: string) {
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+  return base64Regex.test(imageData);
+}
