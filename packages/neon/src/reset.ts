45a1ba417d853eb51be98a99f2a97c4889ff0d0f
kleva-j <kasmickleva@gmail.com>
2023-11-10 22:49:11 +0100

    feat: neon database seeding and reset feature

---
+++ packages/neon/src/reset.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { sql } from "drizzle-orm";
+import { env } from "../env.mjs";
+
+const { log, error } = console;
+
+const client = neon(env.DATABASE_URL);
+const db = drizzle(client);
+
+async function main(): Promise<void> {
+  try {
+    log("RESETING started...");
+
+    await db.execute(sql`DROP TABLE IF EXISTS users CASCADE;`);
+    await db.execute(sql`DROP TABLE IF EXISTS tasks CASCADE;`);
+    await db.execute(sql`DROP TABLE IF EXISTS session CASCADE;`);
+
+    log("RESETING Successful...");
+    log("RESETING Ended...");
+  } catch (err) {
+    error("Error performing reset operation: ", err);
+  } finally {
+    process.exit(0);
+  }
+}
+
+void main();
