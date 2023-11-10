6e7b776783249bd3c5a3131775615371173f8a74
kleva-j <kasmickleva@gmail.com>
2023-11-10 22:49:11 +0100

    feat: neon database seeding and reset feature

--- packages/neon/src/seed.ts
+++ packages/neon/src/seed.ts
@@ -2,21 +2,45 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { faker } from "@faker-js/faker";
 import { env } from "../env.mjs";
-import { users } from "./schema";
+import { type User, users, tasks } from "./schema";
 
 const { log, error } = console;
 
 const db = drizzle(neon(env.DATABASE_URL));
 
-async function main(): Promise<void> {
-  try {
-    log("SEEDING started...");
-    await db.insert(users).values({
+async function generateDefaultUser(): Promise<User> {
+  const [user] = await db
+    .insert(users)
+    .values({
       tenantId: "user_2WrcO7DqoNUE4HxLdZTkYRyRrqp",
       firstName: "Michael",
       lastName: "Obasi",
       email: "kasmickleva@gmail.com",
+    })
+    .returning();
+  return user as User;
+}
+
+async function main(): Promise<void> {
+  try {
+    log("SEEDING started...");
+    const user = await generateDefaultUser();
+    await db.insert(tasks).values({
+      title: faker.lorem.sentence({ min: 3, max: 5 }),
+      label: "bug",
+      status: "todo",
+      priority: "high",
+      authorId: user.tenantId,
+    });
+
+    await db.insert(tasks).values({
+      title: faker.lorem.sentence({ min: 3, max: 5 }),
+      label: "feature",
+      status: "backlog",
+      priority: "medium",
+      authorId: user.tenantId,
     });
     log("SEEDING Successful...");
     log("SEEDING Ended...");
