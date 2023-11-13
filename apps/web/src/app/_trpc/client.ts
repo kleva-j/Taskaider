d711fc282ed6e81d1469b8e0cfc5b858c0297f2d
kleva-j <kasmickleva@gmail.com>
2023-11-13 16:16:07 +0100

    fix: minor fixes

--- apps/web/src/app/_trpc/client.ts
+++ apps/web/src/app/_trpc/client.ts
@@ -1,11 +1,3 @@
-// import { type AppRouter } from "@/server/router";
-
-// import { createTRPCReact } from "@trpc/react-query";
-
-// export const trpc = createTRPCReact<AppRouter>({
-//   abortOnUnmount: true,
-// });
-
 import { type AppRouter } from "@taskaider/api";
 
 import { createTRPCReact } from "@trpc/react-query";
