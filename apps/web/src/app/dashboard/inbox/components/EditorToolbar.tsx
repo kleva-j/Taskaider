5fef3c7fd49ca4599bcac12bbdd3062f9ad80072
kleva-j <kasmickleva@gmail.com>
2023-12-05 18:16:18 +0100

    feat: implement inbox editor component using tiptap

---
+++ apps/web/src/app/dashboard/inbox/components/EditorToolbar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import type { Editor } from "@tiptap/react";
+
+import { Separator, Toggle } from "ui";
+import {
+  Strikethrough,
+  ListOrdered,
+  Heading2,
+  Italic,
+  List,
+  Bold,
+  Code,
+  Quote,
+} from "lucide-react";
+
+type ToolbarProps = { editor: Editor | null };
+
+export const EditorToolbar = ({ editor }: ToolbarProps) => {
+  if (!editor) return null;
+
+  return (
+    <div className="flex border border-input bg-transparent rounded-md h-[35px]">
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("heading")}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 2 }).run()
+        }
+      >
+        <Heading2 className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bold")}
+        onPressedChange={() => editor.chain().focus().toggleBold().run()}
+      >
+        <Bold className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("italic")}
+        onPressedChange={() => editor.chain().focus().toggleItalic().run()}
+      >
+        <Italic className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("strike")}
+        onPressedChange={() => editor.chain().focus().toggleStrike().run()}
+      >
+        <Strikethrough className="h-4 w-4" />
+      </Toggle>
+      <Separator orientation="vertical" className="mx-1" />
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bulletlist")}
+        onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
+      >
+        <List className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("orderedList")}
+        onPressedChange={() => editor.chain().focus().toggleOrderedList().run()}
+      >
+        <ListOrdered className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("blockquote")}
+        onPressedChange={() => editor.chain().focus().toggleBlockquote().run()}
+      >
+        <Quote className="h-4 w-4" />
+      </Toggle>
+      <Separator orientation="vertical" className="mx-1" />
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("codeblock")}
+        onPressedChange={() => editor.chain().focus().toggleCodeBlock().run()}
+      >
+        <Code className="h-4 w-4" />
+      </Toggle>
+    </div>
+  );
+};
