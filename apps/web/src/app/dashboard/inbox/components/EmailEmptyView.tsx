8dbee268e6f80c58506d2e4713acfc9d4a8a8457
kleva-j <kasmickleva@gmail.com>
2023-12-07 00:40:50 +0100

    feat: create floating button to create new mail

--- apps/web/src/app/dashboard/inbox/components/EmailEmptyView.tsx
+++ apps/web/src/app/dashboard/inbox/components/EmailEmptyView.tsx
@@ -1,9 +1,12 @@
+import { FloatingBtn } from "./FloatingBtn";
+
 export const EmailEmptyView = () => {
   return (
     <div className="grid place-items-center h-full">
       <div className="flex justify-center items-center h-full text-gray-500">
         No Email Selected
       </div>
+      <FloatingBtn />
     </div>
   );
 };
---
+++ apps/web/src/app/dashboard/inbox/components/FloatingBtn.tsx
@@ -0,0 +1,16 @@
+import { MailPlus } from "lucide-react";
+import { Button } from "ui";
+
+import Link from "next/link";
+
+export const FloatingBtn = () => (
+  <Button
+    title="Create New"
+    className="fixed z-90 bottom-10 right-8 w-10 h-10 px-0 py-0 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:drop-shadow-xl animate-bounce duration-700"
+    asChild
+  >
+    <Link href="/dashboard/inbox/f/inbox/new">
+      <MailPlus className="text-white" size={20} />
+    </Link>
+  </Button>
+);
