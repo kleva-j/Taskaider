524f714fe4d5afdbad9b77cd9e9744e89a5b7f83
kleva-j <kasmickleva@gmail.com>
2024-10-16 11:58:06 +0100

    config: update siteconfig, tsconfig, vscode and commitlint config

--- apps/web/src/lib/siteconfig.ts
+++ apps/web/src/lib/siteconfig.ts
@@ -1,5 +1,51 @@
+import {
+  FolderKanban,
+  CalendarDays,
+  ListTodo,
+  Inbox,
+  Home,
+} from "lucide-react";
+
 export const siteConfig = {
   links: {
     github: "https://github.com/kleva-j/Taskaider",
   },
 };
+
+export const NavLinks = [
+  {
+    title: "dashboard",
+    icon: Home,
+    label: "home",
+    variant: "default",
+    path: "/dashboard",
+  },
+  {
+    title: "projects",
+    icon: FolderKanban,
+    label: "projects",
+    variant: "default",
+    path: "/projects",
+  },
+  {
+    title: "tasks",
+    icon: ListTodo,
+    label: "tasks",
+    variant: "default",
+    path: "/tasks",
+  },
+  {
+    title: "calendar",
+    icon: CalendarDays,
+    label: "calendar",
+    variant: "default",
+    path: "/calendar",
+  },
+  {
+    title: "inbox",
+    icon: Inbox,
+    label: "inbox",
+    variant: "default",
+    path: "/inbox",
+  },
+];
